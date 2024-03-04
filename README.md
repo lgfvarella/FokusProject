8b61494b2628c4c9905819b0e70e361cea09f051
luis.varella <luis.varella@ufg.br>
2024-03-04 16:47:43 -0300

    concluido

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # FokusProject
+[![NPM](https://img.shields.io/npm/l/react)](https://github.com/lgfvarella/FocusProject/blob/main/LICENSE)
+
+# Sobre o Projeto
+
+https://lgfvarella.github.io/FokusProject/
+
+Fokus Project é um temporizador no estilo pomodoro. #AprendiNa[Alura](https://www.alura.com.br/curso-online-html-css-responsividade-mobile-first)
+
+A aplicação tem a finalidade de ensinar:
+- Métodos de manipulação de elementos no DOM, como estilos CSS, imagens e textos
+- Alterar atributos e classes de forma dinâmica com JavaScript
+- Criar eventos de interação
+- Utilizar objetos utilitários do JavaScript
+
+## Layout Foco
+![Web 1](https://github.com/lgfvarella/FokusProject/blob/main/imagens/FokusProjectFoco.png)
+## Layout Descanso Curto
+![Web 1](https://github.com/lgfvarella/FokusProject/blob/main/imagens/FokusProjectDescansoCurto.png)
+## Layout Descanso Longo
+![Web 1](https://github.com/lgfvarella/FokusProject/blob/main/imagens/FokusProjectDescansoLongo.png)
+
+# Tecnologias utilizadas
+## Front end
+- HTML
+- CSS
+- JavaScript
+
+## Implantação em produção
+- Front end web: GitHub Pages
+
+# Como executar o projeto
+## Front end web
+Pré-requisitos: npm
+
+```bash
+# instalar npm live-server
+npm install -g live-server 
+
+# clonar repositório
+git clone https://github.com/lgfvarella/FokusProject.git
+
+# entrar na pasta do projeto front end web
+cd FokusProject
+
+# executar o projeto
+live-server --port=3000
+
+# acessar pelo navegador
+http://localhost:3000/index.html
+```
+
+# Autor
+
+Luís Gusstavo Fé Varella
+
+<a href = "mailto:lgfvarella@gmail.com" style="border-radius">
+ <img src="https://img.shields.io/badge/Gmail-ff0000?logo=gmail&logoColor=white&style=for-the-badge&labelWidth=120">
+</a>
+<a href = "https://www.linkedin.com/in/lgvarelladevs/" style="border-radius">
+ <img src="https://img.shields.io/badge/Linkedin-0000ff?logo=linkedin&logoColor=white&style=for-the-badge&labelWidth=120">
+</a>
+<a href="https://api.whatsapp.com/send?phone=55062996113999" target="_blank">
+ <img src="https://img.shields.io/badge/WhatsApp-022c02?logo=whatsapp&logoColor=white&style=for-the-badge&labelWidth=120" alt="WhatsApp Badge">
+</a>
+<a href="https://discord.gg/er2hR9BU" target="_blank">
+ <img src="https://img.shields.io/badge/Discord-40128b?logo=discord&logoColor=white&style=for-the-badge&labelWidth=120" alt="Discord Badge">
+</a>
--- script.js
+++ script.js
@@ -3,20 +3,118 @@ const focoBtn = document.querySelector(`.app__card-button--foco`);
 const curtoBtn = document.querySelector(`.app__card-button--curto`);
 const longoBtn = document.querySelector(`.app__card-button--longo`);
 const banner = document.querySelector(`.app__image`);
+const title = document.querySelector(`.app__title`);
+const tempoNaTela = document.querySelector(`#timer`);
+const botoes = document.querySelectorAll(`.app__card-button`);
+const startPauseBt = document.querySelector(`#start-pause`);
+const somPlay = new Audio(`/sons/play.wav`);
+const somPause = new Audio(`/sons/pause.mp3`);
+const somBeep = new Audio(`sons/beep.mp3`);
+const iniciarOuPausarBt = document.querySelector(`#start-pause span`)
+const musicaFocoInput = document.querySelector(`#alternar-musica`);
+const musica = new Audio(`/sons/luna-rise-part-one.mp3`);
+musica.loop = true;
+let tempoDecorridoEmsegundos = 1500;
+let intervaloId = null;
+
+musicaFocoInput.addEventListener(`change`, () =>{
+    musica.paused ? musica.play() : musica.pause();
+})
+
 
 focoBtn.addEventListener(`click`, () => {
-    atribuirContexto(`foco`)
+    tempoDecorridoEmsegundos = 1500;
+    alterarContexto(`foco`);
+    focoBtn.classList.add('active');
+
 })
 
 curtoBtn.addEventListener(`click`, () => {
-    atribuirContexto(`descanso-curto`)
+    tempoDecorridoEmsegundos = 300;
+    alterarContexto(`descanso-curto`);
+    curtoBtn.classList.add('active');
 })
 
 longoBtn.addEventListener(`click`, () => {
-    atribuirContexto(`descanso-longo`)
+    tempoDecorridoEmsegundos = 900;
+    alterarContexto(`descanso-longo`);
+    longoBtn.classList.add('active');
 });
 
-function atribuirContexto(contexto) {
+function alterarContexto(contexto) {
+    mostrarTempo();
+    botoes.forEach((contexto) => {
+        contexto.classList.remove(`active`);
+    });
     html.setAttribute(`data-contexto`, contexto);
     banner.setAttribute(`src`, `/imagens/${contexto}.png`);
+    switch (contexto) {
+        case `foco`:
+
+            title.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            
+            break;
+
+        case `descanso-curto`:
+            title.innerHTML = `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            
+            break;
+
+        case `descanso-longo`:
+
+            title.innerHTML = `Hora de voltar à superfície,<br>
+            <strong class="app__title-strong">Faça uma pausa longa!</strong>`
+            
+            break;
+    
+        default:
+            break;
+    }
 }
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmsegundos <=0 ) {
+        somBeep.play();
+        alert(`Tempo finalizado`);
+        zerar();
+        return
+    }
+    tempoDecorridoEmsegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener(`click`, iniciarOuPausar)
+
+function iniciarOuPausar() { 
+    if(intervaloId){
+        iniciarOuPausarBt.textContent = `Começar`
+        startPauseBt.childNodes[1].src = `/imagens/play_arrow.png`
+        somPause.play(); 
+        
+    } else {
+        iniciarOuPausarBt.textContent = `Pausar` 
+        startPauseBt.childNodes[1].src = `/imagens/pause.png`
+        somPlay.play()
+    }
+    if (intervaloId) {
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva,1000)
+}
+
+function zerar(){
+   clearInterval(intervaloId);
+   
+   intervaloId = null;
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmsegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString(`pt-Br`,{minute: `2-digit`, second: `2-digit`})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo();
